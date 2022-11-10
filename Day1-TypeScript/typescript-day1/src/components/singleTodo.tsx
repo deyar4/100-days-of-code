8fc7331c294a1ff90eb0ed45c5f2a619b70c1868
deyar4 <deyarbakr1997@gmail.com>
2022-11-11 00:16:05 +0300

    added isDone functionality

--- Day1-TypeScript/typescript-day1/src/components/singleTodo.tsx
+++ Day1-TypeScript/typescript-day1/src/components/singleTodo.tsx
@@ -1,6 +1,7 @@
 import { Todo } from "../model";
 import { AiFillEdit, AiFillDelete} from 'react-icons/ai';
 import { MdDone } from 'react-icons/md';
+import TodoList from "./TodoList";
 type Props = {
     todo: Todo;
     todos: Todo[];
@@ -10,9 +11,25 @@ type Props = {
 
 const singleTodo = ({todo, todos, setTodos}: Props) => {
     
+    const handleDone = (id: number) => {
+        setTodos(todos.map((todo) => todo.id === id? {...todo, isDone: !todo.isDone}: todo))
+
+    };
+
+
   return (
     <form className="todosSingle p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-        <span className="todosSingleText mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">{todo.todo}</span>
+        {
+            todo.isDone ? (
+<s className="todosSingleText mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">{todo.todo}</s>
+            ):(
+                <span className="todosSingleText mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">{todo.todo}</span>
+            )
+                
+            
+        }
+        
+       
 
         <div className="flex items-end">
             <span className="mb-2 w-12 h-12 text-gray-500 dark:text-gray-400">
@@ -22,7 +39,7 @@ const singleTodo = ({todo, todos, setTodos}: Props) => {
                 <AiFillDelete size={30} />
             </span>
             <span className="mb-2 w-12 h-12 text-gray-500 dark:text-gray-400">
-                <MdDone size={30} />
+                <MdDone size={30} onClick={() => handleDone(todo.id)} />
             </span>
         </div>
     </form>
